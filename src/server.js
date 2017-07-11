140a825ff6030fb4445ceb3cc6f818f7ec63f51f
anamklone <amk226@pitt.edu>
2017-07-11 02:09:04 -0400

    create server.js

---
+++ src/server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+// Initialize the app
+var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+// REST API DEFINED BELOW
+
+// Generic error handler used by all endpoints
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+/*
+ * "/api/charging_session"
+ *   GET: find all charging sessions
+ *   POST: create a new charging session
+ */
+app.get("/api/charging_session", function(req, res) {
+    console.log("find all charging sessions")
+});
+
+app.post("/api/charging_session", function(req, res) {
+    console.log("create new charging session");
+})
+
+/*
+ * "/api/charging_session/:id"
+ *   GET: find charging session by id
+ *   PUT: update charging session by id
+ *   DELETE: delete charging session by id
+ */
+app.get("/api/charging_session/:id", function(req, res) {
+    console.log("find charging session by id");
+});
+
+app.put("/api/charging_session/:id", function(req, res) {
+    console.log("update charging session by id");
+});
+
+app.delete("/api/charging_session/:id", function(req, res) {
+    console.log("delete charging session by id");
+});
