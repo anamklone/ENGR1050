46091f71fc3e99aa8f80c77999a2b0343f1acb8e
anamklone <amk226@pitt.edu>
2017-07-11 14:53:42 -0400

    update particle firmware code

--- particle/evcharging.ino
+++ particle/evcharging.ino
@@ -3,19 +3,22 @@
 // ----------------------------------------------------------
 
 void setup() {
-    // Declare a Particle.function to trigger new charging session request
-    Particle.function("new-session", newSession);
+    Serial.begin(9600);   // open serial over USB
 
-    // Subscribe to new charging session response event
-    Particle.subscribe(System.deviceID() + "/hook-response/new_session/", newSessionHandler, MY_DEVICES);
+    // Declare a Particle.function to trigger new client instance request
+    Particle.function("new-instance", newInstance);
+
+    // Subscribe to new client instance response event
+    Particle.subscribe(System.deviceID() + "/hook-response/new_instance/", newInstanceHandler, MY_DEVICES);
 }
 
-int newSession(String data) {
+int newInstance(String data) {
     // Trigger the new charging session
-    Particle.publish("new_session", data, PRIVATE);
+    Particle.publish("new_instance", data, PRIVATE);
     return 1;
 }
 
-void newSessionHandler(const char *event, const char *data) {
+void newInstanceHandler(const char *event, const char *data) {
     // Handle the response
+    Serial.println(data);
 }
