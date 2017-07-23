dd0256c3f8c83443746b7413562f7c51f4a99d51
anamklone <amk226@pitt.edu>
2017-07-23 10:23:53 -0400

    import http module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@NgModule({
+    imports: [ HttpModule ]
+})
+
 export class AppComponent {
   title = 'app';
 }
