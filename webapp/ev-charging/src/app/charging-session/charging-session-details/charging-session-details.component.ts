386d9ae6cf6b71fa30f95ec8f3fc7366477986c6
anamklone <amk226@pitt.edu>
2017-07-23 10:03:12 -0400

    rename selector for charging details

--- webapp/ev-charging/src/app/charging-session/charging-session-details/charging-session-details.component.ts
+++ webapp/ev-charging/src/app/charging-session/charging-session-details/charging-session-details.component.ts
@@ -3,7 +3,7 @@ import { ChargingSession } from '../charging-session';
 import { ChargingSessionService } from '../charging-session.service';
 
 @Component({
-    selector: 'app-charging-session-details',
+    selector: 'charging-session-details',
     templateUrl: './charging-session-details.component.html',
     styleUrls: ['./charging-session-details.component.css']
 })
