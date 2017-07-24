5b7d4cf81d02f8a7d9c8980d741ca0b8be6d2802
anamklone <amk226@pitt.edu>
2017-07-24 15:20:37 -0400

    create components for the input and status pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { StatusComponent } from './status/status.component';
 import { ChargingSessionDetailsComponent } from './charging-session/charging-session-details/charging-session-details.component';
 import { ChargingSessionListComponent } from './charging-session/charging-session-list/charging-session-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    InputComponent,
+    StatusComponent,
     ChargingSessionDetailsComponent,
     ChargingSessionListComponent
   ],
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ChargingSession } from '../charging-session/charging-session';
+import { ChargingSessionService } from '../charging-session/charging-session.service';
+
+@Component({
+    selector: 'app-input',
+    templateUrl: './input.component.html',
+    styleUrls: ['./input.component.css']
+    providers: [ChargingSessionService]
+})
+export class InputComponent implements OnInit {
+
+    constructor(private chargingSessionService: ChargingSessionService) { }
+
+    ngOnInit() {
+
+    }
+
+    updateChargingSession() {
+        console.log("updating charging session");
+    }
+}
