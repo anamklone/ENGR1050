5acf8522e37d29213862ffebb62ac8e41760fabe
anamklone <amk226@pitt.edu>
2017-07-25 20:42:28 -0400

    connect next button to update api request

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChargingSession } from '../charging-session/charging-session';
 import { ChargingSessionService } from '../charging-session/charging-session.service';
+import 'rxjs/Rx';
 
 @Component({
     selector: 'app-input',
