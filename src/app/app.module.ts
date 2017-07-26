e648eafafc8efcaf8f31134efb48b1cec0aaad1e
anamklone <amk226@pitt.edu>
2017-07-25 20:08:11 -0400

    connect next button to update api request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
@@ -18,7 +19,8 @@ import { ChargingSessionListComponent } from './charging-session/charging-sessio
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
