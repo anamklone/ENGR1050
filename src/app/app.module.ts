f604b5baf53a0cbe5126853858fe46fca3ac329c
anamklone <amk226@pitt.edu>
2017-07-30 20:42:17 -0400

    attempt to solve errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { StatusComponent } from './status/status.component';
-import { ChargingSessionDetailsComponent } from './charging-session/charging-session-details/charging-session-details.component';
-import { ChargingSessionListComponent } from './charging-session/charging-session-list/charging-session-list.component';
+//import { ChargingSessionDetailsComponent } from './charging-session/charging-session-details/charging-session-details.component';
+//import { ChargingSessionListComponent } from './charging-session/charging-session-list/charging-session-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
     StatusComponent,
-    ChargingSessionDetailsComponent,
-    ChargingSessionListComponent
+    //ChargingSessionDetailsComponent,
+    //ChargingSessionListComponent
   ],
   imports: [
     BrowserModule,
