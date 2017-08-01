dc719f96a510fe578c3be3c94e43f551bad2b07f
anamklone <amk226@pitt.edu>
2017-08-01 03:21:18 -0400

    figure out how to parse incoming data during new session request

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ app.post("/api/charging-session", function(req, res) {
         console.log(data.maxChargeRate);
 
         // Check that all required fields have values
-        if ("undefined" === data.pinId) {
+        if ("undefined" === typeof data.pinId) {
             handleError(res, "invalid input", "must provide pinId", 400);
-        } else if ("undefined" === data.maxChargeRate) {
+        } else if ("undefined" === typeof data.maxChargeRate) {
             handleError(res, "invalid input", "must provide maxChargeRate", 400);
         } else {
 
